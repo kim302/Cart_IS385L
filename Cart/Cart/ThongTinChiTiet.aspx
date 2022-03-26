1f3cfab0030af6d57181bba6a497b1c80b8ec30c
Đặng Công Tấn <congtan2503@gmail.com>
2022-03-26 23:00:25 +0700

    Update

--- Cart/Cart/ThongTinChiTiet.aspx
+++ Cart/Cart/ThongTinChiTiet.aspx
@@ -112,7 +112,7 @@
                     </asp:SqlDataSource>
                 </div>
             </div>
-<<<<<<< HEAD
+            <<<<<<< HEAD
             <div class=" justify-content-right text-left">
                 <asp:DataList ID="DataList2" runat="server" DataSourceID="SqlDataSource1" RepeatColumns="1" RepeatDirection="Horizontal">
                     <ItemTemplate>
@@ -150,7 +150,7 @@
                 </asp:DataList>
                 <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [Masp], [Tensp], [Dongia], [Loaisp], [Hedieuhanh], [Kichthuoc], [Hang], [HinhAnh] FROM [TiVi] WHERE ([Masp] = @Masp)"></asp:SqlDataSource>
             </div>
-=======
+            =======
 >>>>>>> parent of 44fcd5e (Update ba chấm)
         </div>
         </div>
--- Cart/Cart/ThongTinChiTiet.aspx.designer.cs
+++ Cart/Cart/ThongTinChiTiet.aspx.designer.cs
@@ -85,5 +85,23 @@ namespace Cart
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource QUERY_SELECT;
+
+        /// <summary>
+        /// DataList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList2;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
